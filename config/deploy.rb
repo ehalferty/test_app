7a5125d861fc9b37d212122efd16a1d49845bf4b
Ed Halferty <edhalferty@gmail.com>
2014-06-22 23:02:39 -0700

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
-# lock '3.2.1'
-# set :application, 'test-_app'
-# set :repo_url, 'git@example.com:ehalferty/test_app'
-# set :deploy_to, '/srv/rails'
+lock '3.2.1'
+
+set :application, 'test-_app'
+set :repo_url, 'git@example.com:ehalferty/test_app'
+set :deploy_to, '/srv/rails'
+
